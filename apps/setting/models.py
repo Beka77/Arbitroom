12288c86f8bfc2ae6ae6336a2ac290222aa92ea7
alinu1 <uwu1343@gmail.com>
2022-11-28 20:28:07 +0600

    add settings

--- apps/setting/models.py
+++ apps/setting/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Setting(models.Model):
+    title = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to='logo/')
+    descriptiom = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Настройка'
+        verbose_name = 'Настройки'
